44465b914c5d2a8be7cb76de7acb51ebff9e9938
EcranBleu <nclochez@gmail.com>
2021-03-08 13:11:00 +0100

    v1.1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# File_Management
-Basic file management with Python
+# File Management
+
+Basic file management with Python. Includes a small exercise on how to compute the sum of file sizes within a folder.
--- total_file_size_calculator.py
+++ total_file_size_calculator.py
@@ -2,8 +2,8 @@ import os
 
 total_size = 0
 
-for i in os.listdir('C:\\Program Files\\7-Zip'):
-    if os.path.isfile(os.path.join('C:\\Program Files\\7-Zip', i)):
-        total_size += os.path.getsize(os.path.join('C:\\Program Files\\7-Zip', i))
+for i in os.listdir('C:\\exampledir'):
+    if os.path.isfile(os.path.join('C:\\exampledir', i)):
+        total_size += os.path.getsize(os.path.join('C:\\exampledir', i))
 
 print(total_size)
